e6f0a08d46d7d1e4dfb9e393037afd576c27ab6f
shiran-trrzs <shirannnn.4@gmail.com>
2021-11-11 00:20:42 -0500

    Se añadió la vista principal

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
+console.log(data.films[0].title);
 
-console.log(example, data);
+let films = data.films;
+let titleFilm= films.map((f) => f.title);
+let posterFilm= films.map((f) => f.poster);
+let descriptionFilm= films.map((f) => f.description);
+let directorFilm= films.map((f) => f.director);
+let producerFilm= films.map((f) => f.producer);
+let releaseFilm= films.map((f) => f.release_date);
+
+console.log(titleFilm)
+
+function allFilms() {
+    for (let i=0; i<titleFilm.length; i++) {
+        
+    }
+}
+
+
+
