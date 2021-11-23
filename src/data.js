f5fdcaaeb40cdc75ed0ebc5da39d3e4e299ec780
shiran-trrzs <shirannnn.4@gmail.com>
2021-11-23 12:19:02 -0500

    Funciones de export

--- src/data.js
+++ src/data.js
@@ -1,9 +1,37 @@
-// estas funciones son de ejemplo
+import data from './data/ghibli/ghibli.js';
 
-export const example = () => {
-  return 'example';
+export const filterDirector = (nombre) => {
+ return data.films.filter((movie)=>{
+    return movie.director===nombre
+  });
+};
+ 
+export const filterProducer = (nombre) => {
+  return data.films.filter((movie)=>{
+    return movie.producer===nombre
+  });
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+/*export const filterYear = (year) => {
+  return data.films.filter((movie)=>{
+    return movie.release_date===year
+  });
+};*/
+
+export const sortAZ = () => {
+  const newArray = Array.from(data.films);
+  return newArray.sort((nombre, nombre1) => {
+    return (nombre.title < nombre1.title) ? -1 : 1
+  });
 };
+
+export const sortZA = () => {
+  const newArray = Array.from(data.films);
+  return newArray.sort((nombre, nombre1) => {
+    return (nombre.title > nombre1.title) ? -1 : 1
+  });
+};
+
+
+
+
