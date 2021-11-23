be87fc4f71b54e4201956f2b853be8e931c75a1e
shiran-trrzs <shirannnn.4@gmail.com>
2021-11-23 12:16:49 -0500

    Página principal

--- src/main.js
+++ src/main.js
@@ -1,6 +1,57 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {filterDirector, filterProducer, sortAZ, sortZA} from './data.js';
+import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+let films = data.films;
+
+let director = document.getElementById('chooseDirector');
+director.addEventListener('change', () => {
+    let selectedOption = director.value;
+    const result = filterDirector(selectedOption);
+    showCards(result);
+});
+
+let producer = document.getElementById('chooseProducer');
+producer.addEventListener('change', () => {
+    let selectedOption = producer.value;
+    const result = filterProducer(selectedOption);
+    showCards(result);
+})
+
+let sortBy = document.getElementById('sortBy');
+sortBy.addEventListener('change', () => {
+    let selectedOption = sortBy.value;
+    const result = sortAZ(selectedOption);
+    console.log(result)
+    showCards(result);
+})
+
+/*sortBy.addEventListener('change', () => {
+    let selectedOption = sortBy.value;
+    const result = sortZA(selectedOption);
+    showCards(result);
+    console.log(result)
+})*/
+
+
+const showTitle = document.getElementById('showFilms');
+function showCards(allMovies) {
+    showTitle.innerHTML = '';
+    allMovies.forEach((movie) => {
+        const card = document.createElement('div');
+        card.setAttribute('class', 'styleMovie');
+        const info = document.createElement('p');
+        info.setAttribute('class', 'styleTitle');
+        //const info = document.createTextNode(movie.title);
+        info.textContent = `${movie.title}`;
+        //Pintar cards
+        let {poster} = movie;
+        const posterCards = document.createElement('img');
+        posterCards.setAttribute('class', 'styleImg');
+        posterCards.src = poster;
+        showTitle.appendChild(card);
+        card.appendChild(posterCards);
+        card.appendChild(info);
+        })
+}
+showCards(films);
+
